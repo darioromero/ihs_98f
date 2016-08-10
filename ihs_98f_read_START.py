d11cb2c7ae07caf3da52dd462741cffc475a3f3b
darioromero <dromero.canada@gmail.com>
2016-08-09 19:41:15 -0500

    commit -- reformatted file - more info

--- ihs_98f_read_START.py
+++ ihs_98f_read_START.py
@@ -6,17 +6,33 @@ import sys, re, time
 inFile = '/Users/darioromero/Google Drive/IHS/DataFiles/PERMIAN/' + 'PERMMIAN_BASIN_298_Production.98f'
 
 start = time.clock()
-print('Start Time: {0}'.format(start))
+#print('Start Time: {0}'.format(start))
 inFileLines = open(inFile, mode='rt').readlines()
 elapsed = time.clock() - start
 
-pattern = '^END_US_PROD'
-n = 0
+pattern = '^START_US_PROD'
+
+n = 0 # nr. of wells
+m = 0 # nr. of MULTI wells
 
 for line in inFileLines:
     match = re.search(pattern=pattern, string=line, flags=True)
     if (match):
         n += 1
-        print(line)
+    match = re.search(pattern='MULTI', string=line, flags=True)
+    if (match):
+        m += 1
 
-print('Elapsed Time: {0} --- Lines read: {1}'.format(elapsed, n))
+# number of wells to write per file -- wpf
+wpf = 2000
+# nr of files required
+nfiles = int(n / wpf)
+# remaining nr of wells for the extra file
+lstwells = n % wpf
+# print results
+print('Nr of Files with 2000 Wells: {0:0>4} + 1, -- Nr of Wells on Last File: {1:0>6}'.format(nfiles, lstwells))
+print(' --- Wells read: {1:0>8}, MultiWells: {0:0>8}'.format(n, m))
+# Elapsed Time
+print('Elapsed Time: {0}:'.format(elapsed))
+
+
