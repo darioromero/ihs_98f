9556a3bc874716d83a003c72fedbb8bc7c9a1479
darioromero <dromero.canada@gmail.com>
2016-08-09 23:36:34 -0500

    commit -- added cycle, new loop while

--- ihs_98f_read_START.py
+++ ihs_98f_read_START.py
@@ -9,23 +9,13 @@ inFile = '/Users/darioromero/Documents/IHS/Export - 298 Production TEXAS 1-4.98f
 start = time.clock()
 print('Start Time: {0}'.format(start))
 
-pattern = '^END_US_PROD'
-
 n = 0 # nr. of wells
-m = 0 # nr. of MULTI wells
+multi = 0 # nr. of MULTI wells
 
 for line in fileinput.input(inFile):
-    match = re.search(pattern=pattern, string=line, flags=True)
+    match = re.search(pattern='^START_US_PROD', string=line, flags=True)
     if (match):
-        match = re.search(pattern='MULTI', string=line, flags=True)
-        if (match):
-            m += 1
-        else:
-            n += 1
-
-elpsd = time.clock() - start
-# Elapsed Time
-print('Elapsed Time: {0}:'.format(elpsd))
+        n += 1
 
 # number of wells to write per file -- wllspf
 #wllspf = int(input("Enter Proportion of Wells per File: "))
@@ -34,6 +24,39 @@ wllspf = 2000
 nfiles = int(n / wllspf)
 # remaining nr of wells for the extra file
 lastwlls = n % wllspf
+
+cycle = False
+nfile = 0
+wellnr = 0
+
+while wellnr <= n: # instead change this ... loop by cycle
+    ff = open(name='salida.98f', mode='w')
+    for line in fileinput.input(inFile):
+        match = re.search(pattern='^START_US_PROD', string=line, flags=True)
+        if match:
+            wellnr += wellnr  # increment well
+            match = re.search(pattern='MULTI', string=line, flags=True)
+            if match:
+                multi += 1 # is a multi well - discard it
+                break
+            else:
+                cycle = True  # set new cycle START -- END on
+        else:
+            match = re.search(pattern='^END_US_PROD', string=line, flags=True)
+            if match:
+                ff.write(line)
+                cycle = False # last well in the cycle; set cycle off
+        if cycle:
+            ff.write(line) # write line within cycle
+
+
+
+
+
+elpsd = time.clock() - start
+# Elapsed Time
+print('Elapsed Time: {0}:'.format(elpsd))
+
 # print results
 print('Nr of Files with {0} Wells: {1:0>4} + 1, -- Nr of Wells on Last File: {2:0>6}'.format(wllspf, nfiles, lastwlls))
 print('Wells read: {0:0>8}, MultiWells: {1:0>8}'.format(n, m))
