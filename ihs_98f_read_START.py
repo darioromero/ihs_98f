f997d19aea192112733e8e528169d3755135eae1
darioromero <dromero.canada@gmail.com>
2016-08-09 21:37:35 -0500

    commit -- different inFile

--- ihs_98f_read_START.py
+++ ihs_98f_read_START.py
@@ -3,12 +3,13 @@ import sys, re, time, fileinput
 #inFile = ""
 #inFile = input("Enter the input File Name: ")
 
-inFile = '/Users/darioromero/Google Drive/IHS/DataFiles/PERMIAN/' + 'PERMMIAN_BASIN_298_Production.98f'
+#inFile = '/Users/darioromero/Google Drive/IHS/DataFiles/PERMIAN/' + 'PERMMIAN_BASIN_298_Production.98f'
+inFile = '/Users/darioromero/Documents/IHS/Export - 298 Production TEXAS 1-4.98f'
 
 start = time.clock()
 print('Start Time: {0}'.format(start))
 
-pattern = '^START_US_PROD'
+pattern = '^END_US_PROD'
 
 n = 0 # nr. of wells
 m = 0 # nr. of MULTI wells
@@ -16,10 +17,11 @@ m = 0 # nr. of MULTI wells
 for line in fileinput.input(inFile):
     match = re.search(pattern=pattern, string=line, flags=True)
     if (match):
-        n += 1
-    match = re.search(pattern='MULTI', string=line, flags=True)
-    if (match):
-        m += 1
+        match = re.search(pattern='MULTI', string=line, flags=True)
+        if (match):
+            m += 1
+        else:
+            n += 1
 
 elpsd = time.clock() - start
 # Elapsed Time
@@ -35,6 +37,7 @@ lastwlls = n % wllspf
 # print results
 print('Nr of Files with {0} Wells: {1:0>4} + 1, -- Nr of Wells on Last File: {2:0>6}'.format(wllspf, nfiles, lastwlls))
 print('Wells read: {0:0>8}, MultiWells: {1:0>8}'.format(n, m))
+
 
 
 
